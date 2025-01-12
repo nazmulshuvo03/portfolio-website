6865a6c9df0e43ff9d765fa2e8cb72ead8c63246
Nazmul Alom <nazmulshuvo03@gmail.com>
2025-01-12 21:19:11 +0600

    basic page structure created

--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,19 @@
+import json
+from pathlib import Path
+
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    # Path to the JSON file
+    data_file = Path(__file__).resolve().parent.parent / "static/js/data.json"
+
+    # Load JSON data
+    try:
+        with open(data_file, "r") as file:
+            data = json.load(file)
+    except Exception as e:
+        print(f"Error loading JSON data: {e}")
+        data = {}
+
+    return render(request, "home.html", data)
